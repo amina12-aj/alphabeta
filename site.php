c01052051b778e056549d499cacca1f720e74732
amina12-aj <bakareaminata@gmail.com>
2022-05-18 20:22:07 +0100

    first commit

---
+++ site.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ALPHABETA</title>
+</head>
+<body>
+    <form action ="site.php" action= "get">
+   Pick an Alphabet: <input type="text"  name="letter"> <br> 
+   <input type="submit">
+</form>
+<?php 
+   $letter = $_GET["letter"];
+   echo "You choose $letter";
+
+?>
+
+</body>
+</html>
